c5c5b76eb7acec5cce66b26a99639e6ec3c69be0
Ben Chomsang <1994benc@gmail.com>
2020-04-20 21:09:08 +0100

    Added the main chart

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,105 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { ExampleComponent } from 'filterable-line-chart'
+import { FilterableLineChart } from 'filterable-line-chart'
 import 'filterable-line-chart/dist/index.css'
+import { BASE_URL, MY_PUBLIC_DATASET_ID, APIKEY } from './never_commit/keys'
+
+// Mock settings
+// get these from VisSpot's developer console
+const settings = [
+  {
+      "id": "height",
+      "isRequired": true,
+      "name": "Canvas height",
+      "options": [],
+      "rangeEnd": 1000,
+      "rangeStart": 0,
+      "type": "number",
+      "value": 600
+  },
+  {
+      "id": "show_reg_line",
+      "isRequired": true,
+      "name": "Show linear regression line",
+      "options": [],
+      "rangeEnd": 100,
+      "rangeStart": 0,
+      "type": "boolean",
+      "value": true
+  },
+  {
+      "id": "show_line",
+      "isRequired": true,
+      "name": "Show line",
+      "options": [],
+      "rangeEnd": 100,
+      "rangeStart": 0,
+      "type": "boolean",
+      "value": true
+  },
+  {
+      "id": "curve_type",
+      "isRequired": true,
+      "name": "Curve type",
+      "options": [
+          "CurveBasis",
+          "CurveCardinal",
+          "CurveLinear",
+          "CurveCatmullRom (alpha = 0.5)",
+          "CurveCatmullRom (alpha = 1)"
+      ],
+      "rangeEnd": 100,
+      "rangeStart": 0,
+      "type": "options",
+      "value": "CurveLinear"
+  },
+  {
+      "id": "translate_y_label",
+      "isRequired": true,
+      "name": "Translate Y label by",
+      "options": [],
+      "rangeEnd": "500",
+      "rangeStart": "-500",
+      "type": "number",
+      "value": 0
+  }
+]
+
+// Mock data_options
+// get these from VisSpot's developer console
+const data_options = [
+  {
+      "id": "x",
+      "isRequired": true,
+      "name": "X axis",
+      "type": "number",
+      "value": "3.5"
+  },
+  {
+      "id": "y",
+      "isRequired": true,
+      "name": "Y axis",
+      "type": "number",
+      "value": "1.4"
+  }
+]
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [dataset, setdataset] = useState([])
+  useEffect(() => {
+    // Get these API configs from VisSpot's developer console
+    // Currently only open for admins 
+    fetch(
+      `${BASE_URL}/datasets/parse?dataset_id=${MY_PUBLIC_DATASET_ID}&apiKey=${APIKEY}&pagination=0`
+    )
+      .then(function (value) {
+        return value.json()
+      })
+      .then(function (value) {
+        setdataset([...(JSON.parse(value).dataset)])
+      })
+  }, [])
+  return <FilterableLineChart dataset={dataset} settings={settings} dataOptions={data_options} />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,175 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import styles from './styles.module.css'
+import useResizeObserver from 'use-resize-observer/polyfilled'
+import _ from 'lodash'
+import {
+  select,
+  extent,
+  scaleLinear,
+  axisLeft,
+  axisBottom,
+  line,
+  curveBasis,
+  curveCardinal,
+  curveLinear,
+  curveCatmullRom
+} from 'd3'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+const curves = {
+  CurveBasis: curveBasis,
+  CurveCardinal: curveCardinal,
+  CurveLinear: curveLinear,
+  'CurveCatmullRom (alpha = 0.5)': curveCatmullRom.alpha(0.5),
+  'CurveCatmullRom (alpha = 1)': curveCatmullRom.alpha(1)
+}
+
+export const FilterableLineChart = ({
+  dataset,
+  dataOptions,
+  chartId,
+  settings
+}) => {
+  const [width, setwidth] = useState(400)
+  const [height, setheight] = useState(400)
+  const [showRegLine, setshowRegLine] = useState(true)
+  const [showLine, setshowLine] = useState(true)
+  const [curveType, setcurveType] = useState('CurveBasis')
+  const [sortedDataset, setsortedDataset] = useState([])
+  const [translateYLabel, settranslateYLabel] = useState(0)
+  const [xKey, setxKey] = useState('')
+  const [yKey, setyKey] = useState('')
+  const svgRef = useRef()
+
+  const { ref } = useResizeObserver({
+    onResize: ({ width }) => {
+      setwidth(width)
+    }
+  })
+
+  // Setting initial states
+  useEffect(() => {
+    if (
+      !settings ||
+      !dataOptions ||
+      settings.length <= 0 ||
+      dataOptions.length <= 0
+    ) {
+      return
+    }
+    // Settings
+    const heightV = _.find(settings, ['id', 'height'])?.value
+    heightV && setheight(heightV)
+
+    const showRegLineV = _.find(settings, ['id', 'show_reg_line'])?.value
+    showRegLineV && setshowRegLine(showRegLineV)
+
+    const showLineV = _.find(settings, ['id', 'show_line'])?.value
+    showLineV && setshowLine(showLineV)
+
+    const curveTypeV = _.find(settings, ['id', 'curve_type'])?.value
+    curveTypeV && setcurveType(curveTypeV)
+
+    const translateYLabelV = _.find(settings, ['id', 'translate_y_label'])
+      ?.value
+    translateYLabelV && settranslateYLabel(translateYLabelV)
+
+    // Data options
+    const xV = _.find(dataOptions, ['id', 'x'])?.value
+    setxKey(xV)
+    const yV = _.find(dataOptions, ['id', 'y'])?.value
+    setyKey(yV)
+  }, [settings, dataOptions])
+
+  // Sorted the dataset by x axis
+  useEffect(() => {
+    if (!dataset || !xKey || !yKey) {
+      return
+    }
+    const sorted = _.orderBy(dataset, [xKey], ['asc'])
+    setsortedDataset([...sorted])
+  }, [dataset, xKey, yKey])
+
+  // Render chart
+  useEffect(() => {
+    if (!dataset || !sortedDataset || !width || !height || !yKey || !xKey) {
+      return
+    }
+    const xs = _.map(sortedDataset, (item) => parseFloat(item[xKey]))
+    const xExtent = extent(xs)
+    const xScale = scaleLinear().domain(xExtent).range([0, width])
+    const ys = _.map(sortedDataset, (item) => parseFloat(item[yKey]))
+    const yExtent = extent(ys)
+    const yScale = scaleLinear().domain(yExtent).range([height, 0])
+
+    const svg = select(svgRef.current)
+    svg
+      .selectAll('.point')
+      .data(sortedDataset)
+      .join('circle')
+      .attr('class', 'point')
+      .attr('cx', (d) => xScale(parseFloat(d[xKey])))
+      .attr('cy', (d) => yScale(parseFloat(d[yKey])))
+      .attr('fill', 'blue')
+      .attr('r', 5)
+
+    // axises
+    const yAxis = axisLeft(yScale)
+    const xAxis = axisBottom(xScale)
+
+    svg.selectAll('.yaxis').call(yAxis)
+    svg
+      .selectAll('.xaxis')
+      .call(xAxis)
+      .attr('transform', `translate(0,${height})`)
+
+    // lines
+    if (showLine) {
+      const lineGenerator = line()
+        .x((d) => xScale(parseFloat(d[xKey])))
+        .y((d) => yScale(parseFloat(d[yKey])))
+        .curve(curves[curveType])
+      svg
+        .selectAll('.line')
+        .data([sortedDataset])
+        .join('path')
+        .attr('class', 'line')
+        .attr('d', lineGenerator)
+        .attr('stroke-width', 3)
+        .attr('fill', 'none')
+        .attr('stroke', 'pink')
+    } else {
+      svg.selectAll('.line').remove()
+    }
+
+    // reg line
+    if (showRegLine) {
+      // TODO: regline
+    }
+  }, [
+    sortedDataset,
+    width,
+    height,
+    xKey,
+    yKey,
+    showLine,
+    showRegLine,
+    translateYLabel
+  ])
+
+  return (
+    <div
+      ref={ref}
+      style={{
+        width: '100%',
+        height: height,
+        position: 'absolute'
+      }}
+      className={styles.wrapper}
+    >
+      <svg className={styles.svg} width='100%' height='100%' ref={svgRef}>
+        <g className='xaxis' />
+        <g className='yaxis' />
+      </svg>
+    </div>
+  )
 }
